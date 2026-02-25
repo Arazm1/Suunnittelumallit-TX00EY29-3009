33eb66448650c635acc955379728f18fd4da0074
Arazm1 <araz.muhamm3d@gmail.com>
2026-02-25 11:07:45 +0200

    Assignment 6 Done.

--- A6_Decorator/Task/src/EncryptedPrinter.java
+++ A6_Decorator/Task/src/EncryptedPrinter.java
@@ -1,15 +1,70 @@
 package A6_Decorator.Task.src;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import java.util.Base64;
+
 public class EncryptedPrinter extends PrinterDecorator {
+
+    private final SecretKey secretKey;
+    private final String algorithm;
+    private boolean encrypted;
+    
 
     public EncryptedPrinter(Printer printer){
         super(printer);
+        this.algorithm = "AES";
+        this.encrypted = false;
+        this.secretKey = generateKey();
+    }
+
+
+    public static SecretKey generateKey(){
+        try{
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            SecretKey key = keyGenerator.generateKey();
+            return key;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String encryptDecrypt(String text, int mode){
+        try{
+            Cipher cipher = Cipher.getInstance(algorithm);
+            cipher.init(mode, this.secretKey);
+
+            if(mode == Cipher.DECRYPT_MODE){
+                byte[] decodedBytes = Base64.getDecoder().decode(text);
+                byte[] result = cipher.doFinal(decodedBytes);
+                return new String(result);
+            }
+            else{
+                byte[] result = cipher.doFinal(text.getBytes());
+                return Base64.getEncoder().encodeToString(result);
+            }
+        }
+        catch(Exception e){
+            return null;
+        }
     }
 
     @Override
     public void print(String text){
-        //Todo: Functionality
-        super.print(text.toUpperCase());
+        if(!encrypted){
+            text = encryptDecrypt(text, Cipher.ENCRYPT_MODE);
+            encrypted = true;
+        }
+        else{
+            text = encryptDecrypt(text, Cipher.DECRYPT_MODE);
+            encrypted = false;
+        }
+
+        super.print(text);
     }
     
 }
--- A6_Decorator/Task/src/Main.java
+++ A6_Decorator/Task/src/Main.java
@@ -1,5 +1,7 @@
 package A6_Decorator.Task.src;
 
+import javax.crypto.Cipher;
+
 public class Main {
     public static void main(String[] args) {
         
@@ -11,8 +13,16 @@ public class Main {
         printer = new XMLPrinter(new BasicPrinter());
         printer.print("hello world");
 
-        printer = new EncryptedPrinter(new BasicPrinter());
-        printer.print("hello world");
+        EncryptedPrinter encryptedPrinter = new EncryptedPrinter(new BasicPrinter());
+
+        String encrypted = encryptedPrinter.encryptDecrypt("Hello World", Cipher.ENCRYPT_MODE);
+        encryptedPrinter.print("Hello World");
+        encryptedPrinter.print(encrypted);
+
+
+        //encryptedPrinter.print("hello world");
+        //String encryptedText = encryptedPrinter.encryptDecrypt("hello world", Cipher.ENCRYPT_MODE);
+        //encryptedPrinter.print(encryptedText);
 
     }
     
