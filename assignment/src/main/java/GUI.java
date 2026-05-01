979c54ea6ac44bc5d114b1feab6d1507a714c2bc
Arazm1 <araz.muhamm3d@gmail.com>
2026-05-01 10:14:57 +0300

    GUI design improved.

--- assignment/src/main/java/GUI.java
+++ assignment/src/main/java/GUI.java
@@ -11,6 +11,13 @@ public class GUI extends Application {
     private PixelGrid grid = new PixelGrid();
     private Rectangle[][] cells = new Rectangle[8][8];
 
+    private static final String BG_COLOR     = "#0d0d0d";
+    private static final String GRID_COLOR   = "#1a3a1a";
+    private static final String PIXEL_ON     = "#39ff14";
+    private static final String PIXEL_OFF    = "#0d1a0d";
+    private static final String CURSOR_COLOR = "#ffdd00";
+
+
     @Override
     public void start(Stage stage) {
         GridPane root = new GridPane();
@@ -19,17 +26,27 @@ public class GUI extends Application {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Rectangle rect = new Rectangle(40, 40);
-                rect.setStyle("-fx-fill: white; -fx-stroke: black;");
+                //rect.setStyle("-fx-fill: white; -fx-stroke: black;");
+                rect.setStyle(
+                    "-fx-fill: " + PIXEL_OFF + "; " +
+                    "-fx-stroke: " + GRID_COLOR + ";"
+                );
                 cells[i][j] = rect;
                 root.add(rect, j, i);
             }
         }
 
         Button btn = new Button("Create Code");
+        btn.setStyle(
+            "-fx-background-color: #39ff14; " +
+            "-fx-text-fill: #0d0d0d; " +
+            "-fx-font-weight: bold;"
+        );
 
         VBox layout = new VBox(10, root, btn);
 
         Scene scene = new Scene(layout);
+        layout.setStyle("-fx-background-color: " + BG_COLOR + ";");
 
         setupControls(scene, btn);
 
@@ -85,16 +102,28 @@ public class GUI extends Application {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
 
-                if (data[i][j] == 1) {
-                    cells[i][j].setStyle("-fx-fill: black; -fx-stroke: black;");
-                } else {
-                    cells[i][j].setStyle("-fx-fill: white; -fx-stroke: black;");
+                if(data[i][j] == 1){
+                    cells[i][j].setStyle(
+                        "-fx-fill: " + PIXEL_ON + "; " +
+                        "-fx-stroke: " + GRID_COLOR + ";"
+                    );
+                }
+                else{
+                    cells[i][j].setStyle(
+                        "-fx-fill: " + PIXEL_OFF + "; " +
+                        "-fx-stroke: " + GRID_COLOR + ";"
+                    );
                 }
 
                 //Cursor highlight
-                if (i == grid.getCursorRow() && j == grid.getCursorCol()) {
-                    cells[i][j].setStrokeWidth(3);
-                } else {
+                if(i == grid.getCursorRow() && j == grid.getCursorCol()){
+                    cells[i][j].setStyle(
+                        cells[i][j].getStyle() + 
+                        "-fx-stroke: " + CURSOR_COLOR + "; " +
+                        "-fx-stroke-width: 3;"
+                    );
+                }
+                else{
                     cells[i][j].setStrokeWidth(1);
                 }
             }
